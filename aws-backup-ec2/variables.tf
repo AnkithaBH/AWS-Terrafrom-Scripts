dbbea936f2beb3d8a8a982ca7efdf9065fd94b59
Ankitha B H <76145952+AnkithaBH@users.noreply.github.com>
2022-10-05 12:11:44 +0530

    Add files via upload

---
+++ aws-backup-ec2/variables.tf
@@ -0,0 +1,12 @@
+variable "region"{}
+variable "vpc_cidr"{}
+variable "private_subnet_1_CIDR"{}
+variable "private_subnet_1_AZ"{}
+variable "ec2_ami"{}
+variable "instance_type"{}
+variable "ebs_encryption" {}
+variable "ebs_size" {}
+variable "ebs_AZ" {}
+variable "access_key"{}
+variable "secret_access_key"{}
+variable "ebs-vol-type"{}
