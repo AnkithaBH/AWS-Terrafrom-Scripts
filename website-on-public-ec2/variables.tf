4248cb2aea4630ab039dc9db73baf4e4664df558
Ankitha B H <76145952+AnkithaBH@users.noreply.github.com>
2022-10-05 12:15:29 +0530

    Add files via upload

---
+++ website-on-public-ec2/variables.tf
@@ -0,0 +1,16 @@
+variable "region"{}
+variable "vpc_cidr"{}
+variable "public_subnet_CIDR"{}
+variable "public_subnet_AZ"{}
+variable "private_subnet_1_CIDR"{}
+variable "private_subnet_1_AZ"{}
+variable "private_subnet_2_CIDR"{}
+variable "private_subnet_2_AZ"{}
+variable "ec2_ami"{}
+variable "instance_type"{}
+variable "ebs_encryption" {}
+variable "ebs_size" {}
+variable "ebs_AZ" {}
+variable "access_key"{}
+variable "secret_access_key"{}
+variable "ebs-vol-type"{}
