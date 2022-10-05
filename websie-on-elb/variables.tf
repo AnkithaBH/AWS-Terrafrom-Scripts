1e8c2ada119cc1ee45725cb7f4ce7afd7608ea5a
Ankitha B H <76145952+AnkithaBH@users.noreply.github.com>
2022-10-05 12:19:21 +0530

    Add files via upload

---
+++ websie-on-elb/variables.tf
@@ -0,0 +1,18 @@
+variable "region"{}
+variable "vpc_cidr"{}
+variable "public_subnet_1_CIDR"{}
+variable "public_subnet_1_AZ"{}
+variable "public_subnet_2_CIDR"{}
+variable "public_subnet_2_AZ"{}
+variable "private_subnet_1_CIDR"{}
+variable "private_subnet_1_AZ"{}
+variable "private_subnet_2_CIDR"{}
+variable "private_subnet_2_AZ"{}
+variable "ec2_ami"{}
+variable "instance_type"{}
+variable "ebs_encryption" {}
+variable "ebs_size" {}
+variable "ebs_AZ" {}
+variable "ebs-vol-type"{}
+variable "access_key"{}
+variable "secret_access_key"{}
