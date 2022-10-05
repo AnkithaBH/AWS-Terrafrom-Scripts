c037800d8c79136a358f570f89e58ec1b1fc9688
Ankitha B H <76145952+AnkithaBH@users.noreply.github.com>
2022-10-05 12:15:54 +0530

    Update variables.tf

--- website-on-public-ec2/variables.tf
+++ website-on-public-ec2/variables.tf
@@ -11,6 +11,4 @@ variable "instance_type"{}
 variable "ebs_encryption" {}
 variable "ebs_size" {}
 variable "ebs_AZ" {}
-variable "access_key"{}
-variable "secret_access_key"{}
 variable "ebs-vol-type"{}
