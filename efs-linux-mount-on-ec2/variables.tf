d7ee0610b702f1ce79b31caac5b805750556a32d
Ankitha B H <76145952+AnkithaBH@users.noreply.github.com>
2022-10-06 16:52:59 +0530

    Add files via upload

---
+++ efs-linux-mount-on-ec2/variables.tf
@@ -0,0 +1,12 @@
+variable "region"{}
+variable "vpc_cidr"{}
+variable "public_subnet_CIDR"{}
+variable "public_subnet_AZ"{}
+variable "ec2_ami"{}
+variable "instance_type"{}
+variable "ebs_encryption" {}
+variable "ebs_size" {}
+variable "ebs_AZ" {}
+variable "access_key"{}
+variable "secret_access_key"{}
+variable "ebs-vol-type"{}
