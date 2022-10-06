897adb4843498a3d1bd74003febc415a7fa26b92
Ankitha B H <76145952+AnkithaBH@users.noreply.github.com>
2022-10-06 16:53:26 +0530

    Update variables.tf

--- efs-linux-mount-on-ec2/variables.tf
+++ efs-linux-mount-on-ec2/variables.tf
@@ -7,6 +7,4 @@ variable "instance_type"{}
 variable "ebs_encryption" {}
 variable "ebs_size" {}
 variable "ebs_AZ" {}
-variable "access_key"{}
-variable "secret_access_key"{}
 variable "ebs-vol-type"{}
