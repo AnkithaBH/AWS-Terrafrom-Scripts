968b6010a4adbe28024e5e2560ccbe7fb32661b3
Ankitha B H <76145952+AnkithaBH@users.noreply.github.com>
2022-10-15 16:28:07 +0530

    Create variables.tf

---
+++ shared-db-connections-rdsproxy/variables.tf
@@ -0,0 +1,22 @@
+variable "region"{}
+variable "vpc_cidr"{}
+variable "public_subnet_CIDR"{}
+variable "public_subnet_AZ"{}
+variable "private_subnet_1_CIDR"{}
+variable "private_subnet_1_AZ"{}
+variable "ec2_ami"{}
+variable "instance_type"{}
+variable "ebs_encryption" {}
+variable "ebs_size" {}
+variable "ebs_AZ" {}
+variable "ebs-vol-type"{}
+variable "rds-allocated-storage" {}
+variable "rds-db-name" {}
+variable "rds-engine" {}
+variable "rds-engine-version" {}
+variable "rds-instance-class" {}
+variable "rds-username" {}
+variable "rds-password"{}
+variable "rds-storage-type"{}
+variable "rds-public-access"{}
+variable "rds-az"{}
